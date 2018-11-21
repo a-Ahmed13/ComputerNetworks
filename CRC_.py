a8154362e5cad5745cbdd55a7115a24fc1da1e59
abdo ashraf <eng.abdulrahman.ashraf@gmail.com>
2018-11-21 21:31:06 +0200

    Distribution of tasks

---
+++ CRC_.py
@@ -0,0 +1,33 @@
+iput_file="file.txt"
+
+"""
+Abdulrahman Ashraf
+"""
+def lonDev(message,devisor,messageLen,divisorLen):
+    return
+
+"""
+Fouad 
+"""
+def generator():
+    return
+
+"""
+Abdulrahman Ahmed 
+"""
+def verifier():
+    return
+
+def parser():
+    return
+
+"""
+Miechel 
+"""
+def alter(index):
+    return
+    
+
+
+
+
